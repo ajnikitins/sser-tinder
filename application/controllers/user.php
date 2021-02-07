a7f15dbf913368bf3586d944bf39ab5758d450f0
Artūrs Jānis Ņikitins <ajnikitins@gmail.com>
2021-02-08 00:35:21 +0200

    Allow name editing

--- application/controllers/user.php
+++ application/controllers/user.php
@@ -292,6 +292,9 @@ class User extends Website_Controller
 
     public function edit_account()
     {
+        $this->form_validation->set_rules('first_name', 'first_name', 'required');
+        $this->form_validation->set_rules('last_name', 'last_name', 'required');
+
         $config['upload_path'] = './assets/uploads/member/';
         $config['encrypt_name'] = true; // Encrypt filenames uploaded
         $config['allowed_types'] = 'gif|jpg|png|jpeg';
@@ -303,8 +306,10 @@ class User extends Website_Controller
 
         $user = $this->data['user'] = $this->ion_auth->user()->row();
 
-        if ($this->input->post('sex', true)) {
+        if ($this->form_validation->run() === true) {
             $data = array(
+                'first_name' => $this->input->post('first_name', true),
+                'last_name' => $this->input->post('last_name', true),
                 'country' => $this->input->post('country', true),
                 'city' => $this->input->post('city', true),
                 'sex' => $this->input->post('sex', true),
